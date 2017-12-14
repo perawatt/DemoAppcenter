37eb9aef4539c89ed855cdc2661897a9099b4d97
Perawatt <pekung4433@perfenterprise.com>
2017-12-14 15:10:31 +0700

    initial Project
    Signed-off-by: Perawatt <pekung4433@perfenterprise.com>

---
+++ DemoAppcenter/DemoAppcenter/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DemoAppcenter.Models;
+
+namespace DemoAppcenter.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
